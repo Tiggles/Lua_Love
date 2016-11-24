b19574b49e0bb5deba9b99cd16eac26bcd181cf4
Jonas Malte Hinchely <jonas@hinchely.dks>
2016-11-24 18:30:53 +0100

    better controls

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ constBullet = { image = nil, size = 5, width = 5, height = 5, movementSpeed = 50
 constEnemies = { enemy1 = nil, enemy2 = nil, enemy3 = nil }
 
 constEnemies.enemy1 = { image = nil, size = 20, width = 20, height = 20, movementSpeed = 250, shootDelay = 1.2}
-
+score = 0
 lastEnemy = 0
 enemyDelay = 3
 enemiesC = 0
@@ -21,7 +21,7 @@ love.window.setTitle( "Generic Space Shooter" )
 up = 1;
 down = -1;
 collisions = 0;
-acceleration = { speedX = 0, speedY = 0, max = 20, min = 20, delta = 10 }
+acceleration = { speedX = 0, speedY = 0, max = 15, min = -15, delta = 10 }
 
 
 function love.load()
@@ -40,10 +40,13 @@ end
 
 function love.draw()
 	love.graphics.draw(player.image, player.x, player.y, 0, 1, 1, player.width / 2, player.height / 2, 0, 0)
-	love.graphics.printf(table.getn(bullets), 20, 20, 50, "left" )
-	love.graphics.printf(love.timer.getFPS(), 20, 30, 50, "left" )
-	love.graphics.printf(#enemies, 20, 40, 50, "left" )
-	
+	love.graphics.printf(table.getn(bullets), 20, 20, 100, "left" )
+	love.graphics.printf(love.timer.getFPS(), 20, 30, 100, "left" )
+	love.graphics.printf(acceleration.speedX, 20, 50, 100, "left" )
+	love.graphics.printf(acceleration.speedY, 20, 60, 100, "left" )
+
+	love.graphics.printf(score, 20, 70, 50, "left" )
+
 	for i = #enemies, 1, -1 do
 		if (enemies[i].type == 1) then
 			love.graphics.draw(constEnemies.enemy1.image, enemies[i].x, enemies[i].y, 0, 1, 1, enemies[i].size / 2, enemies[i].size / 2, 0, 0)
@@ -59,12 +62,12 @@ function handleInput(delta_time)
 	if love.keyboard.isDown("left") and not love.keyboard.isDown("right") then
 		acceleration.speedX = acceleration.speedX - acceleration.delta * delta_time
 	elseif acceleration.speedX < 0 then 
-		acceleration.speedX = math.min(acceleration.speedX + (acceleration.delta * delta_time) * 1.5, 0)
+		acceleration.speedX = math.min(acceleration.speedX + (acceleration.delta * 2 * delta_time), 0)
 	end
 	if love.keyboard.isDown("right") and not love.keyboard.isDown("left") then
 		acceleration.speedX = acceleration.speedX + (acceleration.delta * delta_time) 
 	elseif acceleration.speedX > 0 then 
-		acceleration.speedX = math.max(acceleration.speedX - (acceleration.delta * delta_time) * 1.5, 0)
+		acceleration.speedX = math.max(acceleration.speedX - (acceleration.delta * 2 * delta_time), 0)
 	end
 
 	if love.keyboard.isDown("escape") then
@@ -77,6 +80,8 @@ function handleInput(delta_time)
 		love.event.quit("restart")
 	end
 
+	acceleration.speedX = math.max(math.min(acceleration.speedX, acceleration.max), acceleration.min)
+	
 	player.x = math.min(math.max(player.x + acceleration.speedX, 0 + player.width / 2), screen.width - player.width / 2)
 	--player.y = math.min(math.max(player.y + acceleration.speedY, 0 + player.height / 2), screen.height - player.height / 2)
 end
@@ -113,6 +118,7 @@ function updateBullets(delta_time)
 				hit = checkBulletCollision(enemies[j], bullets[i])
 				if (hit) then
 					table.remove(enemies, j)
+					score = score + 1 
 				end
 			end
 		end
